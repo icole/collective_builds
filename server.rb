2aaed538de83b8dff0ed5060111ab8580a1b5443
Ian Cole <ian.cole@me.com>
2014-11-17 00:37:05 -0800

    Trying to fix deployment

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@ require 'mongo'
 require 'pry'
 
 get '/' do
-  mongo_uri = ENV['MONGOLAB_URI'] | "localhost"
+  #mongo_uri = ENV['MONGOLAB_URI'] | "localhost"
   #@builds = Mongo::MongoClient.from_uri(mongo_uri).db("collective").collection("builds").find
   @build = []
   erb :index
