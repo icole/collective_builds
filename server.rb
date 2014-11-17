0989c1fde409e57104d4565b6336f0fa62f6e188
Ian Cole <ian.cole@me.com>
2014-11-17 00:35:26 -0800

    Disable mongo

--- server.rb
+++ server.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 get '/' do
   mongo_uri = ENV['MONGOLAB_URI'] | "localhost"
-  @builds = Mongo::MongoClient.from_uri(mongo_uri).db("collective").collection("builds").find
+  #@builds = Mongo::MongoClient.from_uri(mongo_uri).db("collective").collection("builds").find
+  @build = []
   erb :index
 end
