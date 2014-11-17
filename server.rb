3f3f9be82bf14cb49423229b1fa68320255b91a0
Ian Cole <ian.cole@me.com>
2014-11-17 00:54:21 -0800

    Fixing mongo db

--- server.rb
+++ server.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 get '/' do
   mongo_uri = ENV['MONGOLAB_URI']
-  @builds = Mongo::MongoClient.from_uri(mongo_uri).db("collective").collection("builds").find
+  db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
+  @builds = Mongo::MongoClient.from_uri(mongo_uri).db(db_name).collection("builds").find
   erb :index
 end
