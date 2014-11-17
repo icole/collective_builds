00d23a2e0ae7c8afd9778dd6e65b16003840c26b
Ian Cole <ian.cole@me.com>
2014-11-17 00:57:08 -0800

    Insert test crap

--- server.rb
+++ server.rb
@@ -8,3 +8,11 @@ get '/' do
   @builds = Mongo::MongoClient.from_uri(mongo_uri).db(db_name).collection("builds").find
   erb :index
 end
+
+get '/insert_crap' do
+  mongo_uri = ENV['MONGOLAB_URI']
+  db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
+  builds = Mongo::MongoClient.from_uri(mongo_uri).db(db_name).collection("builds")
+  build = {name: 'crap'}
+  builds.insert(build)
+end
